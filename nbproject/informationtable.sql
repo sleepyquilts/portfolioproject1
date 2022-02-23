54e63b22f7b8aed3bc376ca8a01f836139f7848c
sleepyquilts <51676130+sleepyquilts@users.noreply.github.com>
2022-02-22 16:34:11 -0800

    syncing up the files

--- nbproject/informationtable.sql
+++ nbproject/informationtable.sql
@@ -5,47 +5,61 @@ USE portfoliodatabase1;  -- MySQL command
 
 -- create the tables
 CREATE TABLE developer (
-  developerID   INT(11)  NOT NULL   AUTO_INCREMENT,
-  fullname	VARCHAR(50) 	NOT NULL,
+  developerID       mediumint NOT NULL auto_increment,
+  developerName     VARCHAR(255)   NOT NULL,
+  biography			VARCHAR(2000),
   PRIMARY KEY (developerID)
 );
 
+CREATE TABLE skill (
+  skillID        mediumint NOT NULL auto_increment,
+  skill      VARCHAR(255)   NOT NULL,
+  developerID     mediumint,
+  PRIMARY KEY (skillID),
+  FOREIGN KEY (developerID) REFERENCES developer(developerID)
+);
+
 CREATE TABLE education (
-	educationID   INT(11)  NOT NULL   AUTO_INCREMENT,
+	educationID   mediumint NOT NULL auto_increment,
     school VARCHAR(255) NOT NULL,
     major VARCHAR(255) NOT NULL,
-	PRIMARY KEY (educationID)
+    developerID     mediumint,
+	PRIMARY KEY (educationID),
+    FOREIGN KEY (developerID) REFERENCES developer(developerID)
 );
-
-CREATE TABLE skills (
-	skillsID INT(11)  NOT NULL   AUTO_INCREMENT,
-    skill VARCHAR(255) NOT NULL,
-    PRIMARY KEY (skillsID)
-);
-
 
 -- insert data into the database
-INSERT INTO developer VALUES
-(1,'Carlo Santos'),
-(2, 'Jennah Hubenak'),
-(3, 'Taylor Hillis'),
-(4, 'Tamara Schrader');
-INSERT INTO education VALUES
-(1,'OLYMPIC COLLEGE','INFORMATION SYSTEMS'),
-(2, 'OLYMPIC COLLEGE','INFORMATION SYSTEMS'),
-(3,'OLYMPIC COLLEGE','INFORMATION SYSTEMS'),
-(4,'OLYMPIC COLLEGE','INFORMATION SYSTEMS');
-INSERT INTO skills VALUES
-(1,'HTML5/CSS'),
-(2,'HTML5/CSS'),
-(3,'HTML5/CSS'),
-(4,'HTML5/CSS');
 
-SELECT *
-FROM developer
-	INNER JOIN education
-	ON education.educationID = developer.developerID
-    INNER JOIN skills
-    ON skills.skillsID = developer.developerID;
-    
-    
+
+INSERT INTO developer (developerName, biography) VALUES
+('Carlo Santos', 'I am a Web Developer and am always looking to learn new skills. I have a background in communications and customer service. My many years of customer service have taught me how to effectively interact with stakeholders, how to be a team player and how to fully contribute to the success of a company. My current role as a Web Developer also has me working in the marketing team. I have been in charge of the online presence of new product integrations, promotions and sales in the US and EMEA regions. I look forward to working with all future clients.'),
+('Jennah Hubenak', 'I am a Web Developer.  I am always looking for ways to learn new skills and hobbies.  I am an avid quilter and I have a larger sewing machine collection than I would like to admit.');
+-- ('Taylor Hillis'),
+-- ('Tamara Schrader');
+ 
+INSERT INTO skill (skill, developerID) VALUES
+('Project Management', 1),
+('HTML5/CSS', 1),
+('JavaScript', 1),
+('MongoDB', 1),
+('Github', 1),
+('jQuery', 1),
+('PostgreSQL', 1),
+('Node.js', 1),
+('Express', 1),
+('Angular', 1),
+('Project Management', 2),
+('HTML5/CSS', 2),
+('JavaScript', 2),
+('Github', 2),
+('Cisco', 2);
+
+INSERT INTO education (school, major, developerID) VALUES
+('Olympic College','Information Systems', 1),
+('University of Washington','Communication', 1),
+('Olympic College','Information Systems', 2),
+('The University of Texas','Economics', 2);
+-- ('OLYMPIC COLLEGE','INFORMATION SYSTEMS', 3),
+-- ('OLYMPIC COLLEGE','INFORMATION SYSTEMS', 4);
+
+SELECT * FROM developer;
---
+++ tamara.php
@@ -0,0 +1,124 @@
+
+
+
+
+<!doctype html>
+
+<html lang="en">
+<head>
+
+	<title>Individual page | Tamara Schrader</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">	
+	<link rel="shortcut icon" href="images/favicon.ico">
+	<link rel="stylesheet" href="styles/normalizet.css">
+    <link rel="stylesheet" href="styles/speakert.css">
+    <link rel="stylesheet" href="styles/slicknavt.css">
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&family=Source+Code+Pro:ital,wght@1,900&display=swap"
+        rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&family=Source+Code+Pro:ital,wght@1,900&display=swap"
+        rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="js/jquery.slicknav.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#nav_menu').slicknav({
+                prependTo: "#mobile_menu"
+            });
+        });
+    </script>    
+</head>
+	
+<body>
+	<header>
+		<h2>Nerd Crew</h2>
+		<h3>Portfolio</h3>
+    </header>
+       <nav id="mobile_menu"></nav>
+       <nav id="nav_menu">
+    	<ul>
+    		<li><a href="index.html">Home</a></li>
+    		<li><a  href="about.html">About</a></li>
+            <li><a href="work.html">Work</a></li>
+    		<li><a href="contact.html">Contact</a></li>        
+    	</ul>
+    </nav>
+    <main>
+        <article>
+          <h1>Tamara</h1>
+          <img src="images/tamara.jpg" alt="Tamara Schrader"/>
+          <p>I'm an aspiring IT professional who loves to learn and help people with technology. 
+		     I have 20 years of experience working in customer service and as part of a team.    
+		     I enjoy technology through building computers, troublshooting problems for friends, coding websites, and researching new platforms.  
+		     <p> A few of my accomplishments are: </p>    
+          <p>Created a HTML portfolio website</p>
+          <p>Developed code for mini-games</p>
+          <p>Developed a restaurant website</p>
+        </article>
+        <aside>
+          <h2>Qualities</h2>
+          <nav>
+            <ul>
+              <li>Energetic</a></li>
+              <li>Team player</a></li>
+              <li>Honest</a></li>
+              <li>Problem Solver</a></li>
+              <li>Punctual</a></li>
+              <li>Loyal</a></li>
+            </ul>
+			<p>
+			<h4>
+			 I have training and experience in the following areas:
+			 </h4>
+			 <ul>
+			 <li>
+                Project Management
+                </li>
+                <li>
+                    HTML5/CSS
+                </li>
+                <li>
+                    JavaScript
+                </li>
+                <li>
+                    Networking
+                </li>
+                <li>
+                    Github
+                </li>
+                <li>
+                    Cisco
+                </li>
+				</ul>
+                </p>
+				
+          </nav>
+		  <h1>
+			 <a class="icon-link" href="https://www.facebook.com/tamara.schrader/" target="_blank">
+                    <ion-icon name="logo-facebook"></ion-icon>
+                </a>
+				<a class="icon-link" href="https://github.com/tamela13" target="_blank">
+                    <ion-icon name="logo-github"></ion-icon>
+					</a>
+					</h1>
+		  
+        </aside>
+		
+    </main>
+	<footer>
+	 <p>Olympic College BAS-IS Class 2021</p>
+        <p><a target="_blank" href="https://icons8.com/icon/2753/nerd">Nerd</a> icon by <a target="_blank"
+                href="https://icons8.com">Icons8</a></p>
+      
+    </footer>
+	 <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <script src="js/advice.js"></script>
+</body>
+</html>
